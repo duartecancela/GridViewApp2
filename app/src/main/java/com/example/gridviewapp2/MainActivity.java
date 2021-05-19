db091a2e62c0bb0e81ce1ac4ebfbc51fad2a7156
Duarte Cancela <duartecancela@gmail.com>
2021-05-19 15:54:22 +0100

    Implement single view image on click

--- app/src/main/java/com/example/gridviewapp2/MainActivity.java
+++ app/src/main/java/com/example/gridviewapp2/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.gridviewapp2;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
 import android.view.Menu;
+import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
@@ -13,5 +19,16 @@ public class MainActivity extends Activity {
 
         GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id){
+                // Send intent to SingleViewActivity
+                Intent i = new Intent(getApplicationContext(), SingleViewActivity.class);
+                // Pass image index
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
     }
 }
